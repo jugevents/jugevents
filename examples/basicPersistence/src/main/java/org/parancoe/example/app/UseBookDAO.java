dcbebb8466abd163ffe97ab46c3de16327493730
egesuato <devnull@localhost>
2007-02-14 20:23:01 +0000

    Changed attribute "mumPages" in "numPages". Added a book example.

--- examples/basicPersistence/src/main/java/org/parancoe/example/app/UseBookDAO.java
+++ examples/basicPersistence/src/main/java/org/parancoe/example/app/UseBookDAO.java
@@ -15,7 +15,7 @@ import org.springframework.beans.factory.access.SingletonBeanFactoryLocator;
 /**
  * A simple example of an application using Parancoe persistence module for Book entity.
  * @author <a href="mailto:enricogiurin@gmail.com">Enrico Giurin</a>
- * @version $Revision: fe9e0a88141e $
+ * @version $Revision: 7f212cef9533 $
  *
  */
 public class UseBookDAO {
@@ -24,24 +24,26 @@ public class UseBookDAO {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		  BeanFactoryLocator bfl = SingletonBeanFactoryLocator.getInstance("beanRefFactory.xml");
-	      BeanFactoryReference bf = bfl.useBeanFactory("org.parancoe.example");
+		BeanFactoryLocator bfl = SingletonBeanFactoryLocator.getInstance("beanRefFactory.xml");
+	    BeanFactoryReference bf = bfl.useBeanFactory("org.parancoe.example");
 	      
-	      //code added by enrico
+	    //code added by enrico & emanuele
 	        
-	        BookBO bookBO =  (BookBO)bf.getFactory().getBean("bookBO");
-	        BookDao bookDao = bookBO.getDao();
-	        Book myBook = new Book("Parancoe Team", "Parancoe: the best framework");
+	    BookBO bookBO =  (BookBO)bf.getFactory().getBean("bookBO");
+	    BookDao bookDao = bookBO.getDao();
+	    Book myBook = new Book("Parancoe Team", "Parancoe: the best framework");
 	        
-	        Book myBook2 = new Book("Mario Rossi", "My life");
-	        Book myBook3 = new Book("Parancoe Team", "Parancoe: tips & trick");
+	    Book myBook2 = new Book("Mario Rossi", "My life");
+	    Book myBook3 = new Book("Parancoe Team", "Parancoe: tips & trick");
+	    Book myBook4 = new Book("The Hitchhiker's Guide to the Galaxy", "Douglas Adams", 320);
 	        
-	        bookDao.create(myBook);
-	        bookDao.create(myBook2);
-	        bookDao.create(myBook3);
+	    bookDao.create(myBook);
+	    bookDao.create(myBook2);
+	    bookDao.create(myBook3);
+	    bookDao.create(myBook4);
 	        
-	        List<Book> myBooks= bookDao.findByAuthor("Parancoe Team");
-	        System.out.println(myBooks.size());
+	    List<Book> myBooks= bookDao.findByAuthor("Parancoe Team");
+	    System.out.println(myBooks.size());
 	     
 
 	}
