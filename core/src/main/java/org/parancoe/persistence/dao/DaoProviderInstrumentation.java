6a8b7265882671933c0cde56f120561ec75f8fa6
benfante <devnull@localhost>
2007-06-04 07:13:43 +0000

    Fixed error messages.

--- core/src/main/java/org/parancoe/persistence/dao/DaoProviderInstrumentation.java
+++ core/src/main/java/org/parancoe/persistence/dao/DaoProviderInstrumentation.java
@@ -27,7 +27,7 @@ import org.springframework.util.StringUtils;
  * An aspect for instrumenting a dao provider.
  *
  * @author <a href="mailto:lucio.benfante@jugpadova.it">Lucio Benfante</a>
- * @version $Revision: 8c19c37e30b9 $
+ * @version $Revision: 8e22dad77db1 $
  */
 @Aspect()
 public class DaoProviderInstrumentation {
@@ -50,7 +50,8 @@ public class DaoProviderInstrumentation {
                     result = pjp.proceed(args);
                 } catch (Throwable throwable) {
                     logger.error("No "+daoNameFromMethod(method)+ "DAO in the daoMap. " +
-                            "Trying to call "+method.getName()+" method, but the method doesn't exist in the object.");
+                            "Trying to call "+method.getName()+" method, but the method doesn't exist in the object."
+                            , throwable);
                     throw throwable;
                 }
             }
@@ -60,7 +61,8 @@ public class DaoProviderInstrumentation {
                 result = pjp.proceed(args);
             } catch (Throwable throwable) {
                     logger.error("Method not starting with \"get\". " +
-                            "Trying to call "+method.getName()+" method, but the method doesn't exist in the object ("+target.getClass().getName()+").");
+                            "Trying to call "+method.getName()+" method, but the method doesn't exist in the object ("+target.getClass().getName()+")."
+                            , throwable);
                 throw throwable;
             }
         }
