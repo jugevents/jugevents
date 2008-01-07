94708e888e93e7e4740ccbc7ef97233d0e716254
svnsync <devnull@localhost>
2008-01-07 22:43:40 +0000

    Wrong import

--- parancoe-yaml/src/test/java/org/parancoe/yaml/Array.java
+++
@@ -1,7 +0,0 @@
-package org.parancoe.yaml;
-
-public class Array {
-    public int[] dims;
-
-    public String name;
-}
--- parancoe-yaml/src/test/java/org/parancoe/yaml/UnionSet.java
+++
@@ -1,7 +0,0 @@
-package org.parancoe.yaml;
-
-public class UnionSet {
-    public int dim;
-
-    public int[][][] constraints;
-}
