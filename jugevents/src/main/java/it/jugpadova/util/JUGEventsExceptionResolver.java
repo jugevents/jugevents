968c80769cb01ae3b93755227bad826d101b3157
lucio.benfante <devnull@localhost>
2008-02-21 05:02:41 +0000

    Fixed message generation for exceding upload max size in the exception resolver.

--- jugevents/src/main/java/it/jugpadova/util/JUGEventsExceptionResolver.java
+++ jugevents/src/main/java/it/jugpadova/util/JUGEventsExceptionResolver.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package it.jugpadova.util;
 
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.log4j.Logger;
 import org.parancoe.web.ExceptionResolver;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -23,13 +24,23 @@ public class JUGEventsExceptionResolver extends ExceptionResolver {
     private static final Logger logger =
             Logger.getLogger(JUGEventsExceptionResolver.class);
 
+    private CommonsMultipartResolver multipartResolver;
+
+    public CommonsMultipartResolver getMultipartResolver() {
+        return multipartResolver;
+    }
+
+    public void setMultipartResolver(CommonsMultipartResolver multipartResolver) {
+        this.multipartResolver = multipartResolver;
+    }
+
     @Override
     public ModelAndView resolveException(HttpServletRequest req, HttpServletResponse res, Object object, Exception e) {
         if (e instanceof ParancoeAccessDeniedException) {
             return new ModelAndView("accessDenied", null);
         }
         if (e instanceof MaxUploadSizeExceededException) {
-            return Utilities.getMessageView("upload.maximumSizeExceeded", "100000");
+            return Utilities.getMessageView("upload.maximumSizeExceeded", Long.toString(multipartResolver.getFileUpload().getSizeMax()));
         }
         logger.error("Unexpected exception", e);
         return super.resolveException(req, res, object, e);
