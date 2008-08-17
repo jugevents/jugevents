cc532787c48f0fc823a8ce955496573fce7cb46c
lucio.benfante <devnull@localhost>
2008-08-17 06:01:24 +0000

    Added tests on ReliabilityEditController (to use as examples for future tests)

--- jugevents/src/test/java/it/jugpadova/controllers/ReliabilityEditControllerTest.java
+++ jugevents/src/test/java/it/jugpadova/controllers/ReliabilityEditControllerTest.java
@@ -3,14 +3,15 @@ package it.jugpadova.controllers;
 import it.jugpadova.JugEventsBaseTest;
 import it.jugpadova.dao.JuggerDao;
 import it.jugpadova.po.Jugger;
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
-import org.parancoe.web.test.ControllerTest;
+import javax.servlet.http.HttpServletResponse;
+import org.parancoe.web.annotation.ParancoeAnnotationMethodHandlerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.validation.BeanPropertyBindingResult;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.support.SessionStatus;
-import org.springframework.web.bind.support.SimpleSessionStatus;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author Enrico Giurin
@@ -21,24 +22,35 @@ public class ReliabilityEditControllerTest extends JugEventsBaseTest {
     private ReliabilityEditController controller;
     @Autowired
     private JuggerDao juggerDao;
+    @Autowired
+    private ParancoeAnnotationMethodHandlerAdapter methodHandler;
 
     public void testConfiguration() {
         assertNotNull(controller);
     }
     
-    public void testUpdate() {
+    public void testUpdate() throws Exception {
         HttpServletRequest req = new MockHttpServletRequest("POST", "/adminjugger/reliability.form");
+        HttpServletResponse res = new MockHttpServletResponse();
         Jugger jugger = juggerDao.findByEmail("fabrizio@gianneschi.it");
-        BindingResult result = new BeanPropertyBindingResult(jugger, "jugger");
-        SessionStatus status = new SimpleSessionStatus();
-        String view = controller.update(req, jugger, result, status);
-        assertEquals("redirect:/adminjugger/juggersearch.form", view);
+        req.getSession().setAttribute("jugger", jugger);
+        ModelAndView mv = methodHandler.handle(req, res, controller);
+        assertEquals("redirect:/adminjugger/juggersearch.form", mv.getViewName());
     }
     
-//    public void testGET() throws Exception {
-//        req = new MockHttpServletRequest("GET", "/adminjugger/reliability.form");
-//        req.setParameter("jugger.user.username", "enrico");
-//        ModelAndView mv = controller.handleRequest(req, res);
-//
-//    }
+    public void testForm() throws Exception {
+        MockHttpServletRequest req = new MockHttpServletRequest("GET", "/adminjugger/reliability.form");
+        req.setParameter("jugger.user.username", "enrico");
+        HttpServletResponse res = new MockHttpServletResponse();
+        ModelAndView mv = methodHandler.handle(req, res, controller);
+        Object oJugger = mv.getModel().get("jugger");
+        assertNotNull(oJugger);
+        assertTrue("The object is not of type Jugger", oJugger instanceof Jugger);
+        Object sJugger = req.getSession().getAttribute("jugger");
+        assertNotNull(sJugger);
+        assertTrue("The object is not of type Jugger", sJugger instanceof Jugger);
+        assertSame(oJugger, sJugger);
+        assertEquals("enrico", ((Jugger)oJugger).getUser().getUsername());
+        assertEquals("jugger/admin/reliability", mv.getViewName());
+    }
 }
