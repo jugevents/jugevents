0544ecea4f5a97b00d5fdec5c8c131efa782bfab
lucio.benfante <devnull@localhost>
2009-04-12 10:11:24 +0000

    Changed the Textile rendering library

--- jugevents/src/main/java/it/jugpadova/blo/FilterBo.java
+++ jugevents/src/main/java/it/jugpadova/blo/FilterBo.java
@@ -5,7 +5,7 @@ import org.directwebremoting.WebContext;
 import org.directwebremoting.WebContextFactory;
 import org.directwebremoting.proxy.dwr.Util;
 
-import com.plink.plextile.TextParser;
+import it.jugpadova.util.TextFilterUtils;
 import org.directwebremoting.annotations.RemoteMethod;
 import org.directwebremoting.annotations.RemoteProxy;
 import org.springframework.stereotype.Component;
@@ -17,8 +17,6 @@ import org.springframework.stereotype.Component;
 @Component
 @RemoteProxy(name="filterBo")
 public class FilterBo {
-
-    private static TextParser textParser = new TextParser();
 
     public FilterBo() {
     }
@@ -37,7 +35,7 @@ public class FilterBo {
     public static String filterText(String text, String filter,
             boolean escapeXml) {
         // Only Textile for now
-        String result = textParser.parseTextile(text, !escapeXml);
+        String result = TextFilterUtils.formatText(text, TextFilterUtils.TEXTILE_FILTER);
         return result;
     }
 }
